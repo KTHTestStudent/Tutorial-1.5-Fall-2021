c8870bd7bc2c5926bcf1e9b2386b3b590da3ce0e
KTHTestStudent <knight@ucf.edu>
2021-09-29 14:11:37 -0400

    animation stuff

--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -10,6 +10,20 @@ public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
 
+Animator anim;
+
+
+void Start()
+
+{
+
+  anim = GetComponent<Animator>();
+
+}
+
+
+
+
 // Update is called once per frame
 void Update()
 {
@@ -17,6 +31,8 @@ void Update()
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
+          
+          anim.SetInteger("State", 1);
 
          }
 
@@ -24,17 +40,25 @@ void Update()
         {
           musicSource.Stop();
 
+          
+          anim.SetInteger("State", 0);
+
          }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+
+          
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
+          
+          anim.SetInteger("State", 0);
 
          }
 
